a3923de9ca3b2fb607c15e522e8768d11ddcf8b1
Ubair <113605539+Blue-pill-786@users.noreply.github.com>
2026-02-20 06:56:33 +0530

    Dashboard

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import webhookRoutes from "./routes/webhook.routes.js";
 
+import adminRoutes from "./routes/admin.routes.js";
+
 const app = express();
 
 app.use(express.json());
@@ -9,6 +11,7 @@ app.get("/", (req, res) => {
   res.send("WhatsApp Automation Server is running 🚀");
 });
 app.use(express.static("public"));
+app.use(adminRoutes);
 
 app.use("/", webhookRoutes);
 
