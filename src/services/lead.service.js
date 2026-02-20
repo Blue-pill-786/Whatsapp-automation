d781c5ef4cd65da5feace69c25f424d2bd642fef
Ubair <113605539+Blue-pill-786@users.noreply.github.com>
2026-02-20 05:51:22 +0530

    Update lead.service.js

--- src/services/lead.service.js
+++ src/services/lead.service.js
@@ -1,6 +1,10 @@
 import { google } from "googleapis";
 
 export async function saveLead({ clientName, phone, step, message }) {
+  console.log("EMAIL:", process.env.GSHEET_CLIENT_EMAIL);
+  console.log("KEY:", process.env.GSHEET_PRIVATE_KEY);
+  console.log("KEY LENGTH:", process.env.GSHEET_PRIVATE_KEY?.length);
+  console.log("SHEET:", process.env.GSHEET_ID);
   const auth = new google.auth.JWT(
     process.env.GSHEET_CLIENT_EMAIL,
     null,
